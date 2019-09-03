6b7fe52e79225300d0dac40fc54ff0ce464fce52
Richard Yamamoto <rybolteri@gmail.com>
2019-09-03 11:25:38 -0300

    chore: Promise

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.date,
+      loading: true,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository</h1>;
+  }
 }
